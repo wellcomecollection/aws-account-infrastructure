6f39c826b3e997abc383901ceb49ebf038dd48e6
Alex Chan <alex@alexwlchan.net>
2023-08-13 21:26:29 +0100

    remove two unused provider blocks

--- accounts/microsites/provider.tf
+++ accounts/microsites/provider.tf
@@ -5,12 +5,3 @@ provider "aws" {
     role_arn = "arn:aws:iam::782179017633:role/microsites-admin"
   }
 }
-
-provider "aws" {
-  region = "eu-west-1"
-  alias  = "platform"
-
-  assume_role {
-    role_arn = "arn:aws:iam::760097843905:role/platform-admin"
-  }
-}
