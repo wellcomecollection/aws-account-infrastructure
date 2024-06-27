6c13896cdf6e9554808a0881819fa6519281944b
Robert Kenny <r.kenny@wellcome.org>
2024-06-27 14:41:29 +0100

    narrow permissions for experience role
    Co-Authored-By: Raphaelle Cantin <110461050+rcantin-w@users.noreply.github.com>

--- accounts/experience/iam_experience_gha.tf
+++ accounts/experience/iam_experience_gha.tf
@@ -1,19 +1,32 @@
-module "s3_dash_write_assume_role" {
+module "experience_gha" {
   source          = "../../modules/github_repo_role"
   policy_document = data.aws_iam_policy_document.github_actions_assume_role_policy.json
   github_repositories = [
     "wellcomecollection/wellcomecollection.org",
   ]
-  role_name                = "s3_dash_write"
+  role_name                = "experience"
   github_oidc_provider_arn = module.experience_account.openid_connect_provider_arn
 }
 
-// We allow the GitHub Actions role to assume the appropriate role to perform build actions
 data "aws_iam_policy_document" "github_actions_assume_role_policy" {
+
+  # Below here are actions for deploying the prismic linting project int he experience repo
   statement {
-    actions = ["s3:*"]
+    actions = [
+      "s3:putObject",
+    ]
     resources = [
-      "arn:aws:s3:::dash.wellcomecollection.org"
+      "arn:aws:s3:::dash.wellcomecollection.org/prismic-linting/report.json"
+    ]
+  }
+
+  statement {
+    actions = [
+      "cloudfront:CreateInvalidation",
+    ]
+
+    resources = [
+      "arn:aws:cloudfront::${local.account_id}:distribution/EIOS79GG23UUY",
     ]
   }
 }
