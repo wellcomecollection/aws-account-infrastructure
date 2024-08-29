cb7e1e404161690390db684f24a4c0e036b0be93
Agnes Garoux <a.garoux@wellcome.ac.uk>
2024-08-29 14:32:49 +0100

    rename the github secret + how to generate PTA

--- accounts/platform/gha_org_secrets.tf
+++ accounts/platform/gha_org_secrets.tf
@@ -9,8 +9,8 @@ terraform {
 // creates a gha secret at the org level
 
 // secret value for role-to-assume in workflows that need to pull from ecr
-resource "github_actions_organization_secret" "gha_platform_ecr_read_role_arn" {
-  secret_name     = "GHA_PLATFORM_ECR_READ_ROLE_ARN"
+resource "github_actions_organization_secret" "gha_scala_formatting_role_arn" {
+  secret_name     = "GHA_SCALA_FORMATTING_ROLE_ARN"
   visibility      = "private"
-  plaintext_value = module.platform_ecr_read_assume_role.role_arn
+  plaintext_value = module.gha_scala_formatting_role.role_arn
 }
--- accounts/platform/provider.tf
+++ accounts/platform/provider.tf
@@ -17,7 +17,10 @@ provider "aws" {
 }
 
 # Configure the GitHub Provider
-# Create a fine-grained personal access token in Github and 
+# Create a fine-grained personal access token in Github:
+# Go to your Github account > Settings > Developer settings > PAT > Fine-grained tokens
+# Give it a name, description and a short (7 days) expiration
+# In Organization permissions select Secret - Manage Actions organization secrets: read and write
 # export TF_VAR_github_token=<your-token-here> before applying the tf
 provider "github" {
   owner = "wellcomecollection" 
